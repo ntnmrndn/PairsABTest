921b68d42f9dd76b6be37093a4c5f00ae6424dd7
Antoine Marandon <antoine@marandon.fr>
2023-11-20 15:19:25 +0900

    Try to fix build

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "PairsABTest",
-    platforms: [.macOS(.v10_15), .iOS(.v15), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [.iOS(.v15), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
